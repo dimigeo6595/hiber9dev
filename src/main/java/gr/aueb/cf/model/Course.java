e2bec16d3ddb342ac325552dcf66c706f6469d27
GiorgosDimitriadis <dimigeo6595@gmail.com>
2026-01-20 20:49:08 +0200

    feat: add model with Course and Teacher classes

---
+++ src/main/java/gr/aueb/cf/model/Course.java
@@ -0,0 +1,40 @@
+package gr.aueb.cf.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "courses")
+public class Course {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "title", length = 255, nullable = false, unique = true)
+    private String title;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "teacher_id")
+    private Teacher teacher;
+
+    public Course(){
+
+    }
+
+    public Course(String title){
+        this.title = title;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Teacher getTeacher() {
+        return teacher;
+    }
+
+    public void setTeacher(Teacher teacher) {
+        this.teacher = teacher;
+    }
+}
+
